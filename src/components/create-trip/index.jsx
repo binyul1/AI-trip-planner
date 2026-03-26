4dd00130b912044685c244d948e759900eae19f9
binyul <jiibinyul@gmail.com>
2026-03-26 21:11:14 +0545

    complete Trip form

--- src/components/create-trip/index.jsx
+++ src/components/create-trip/index.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import React from "react";
+import GooglePlacesAutocomplete from "react-google-places-autocomplete";
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { SelectBudgetOptions, SelectTravelesList } from "../../constants/options";
+import { Button } from "../ui/button";
 
 function CreateTrip() {
+  const [place, setPlace] = useState();
   return (
-    <div>CreateTrip</div>
-  )
+    <div className="sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10">
+      <h2 className="font-bold text-3xl">
+        Tell us your travel preferences 🏕️🌴
+      </h2>
+      <p className="mt-3 text-gray-500 text-xl">
+        Just provide some basic information, and our trip planner will generate
+        a customized itinerary based on your preferences. What is destination of
+        choice?
+      </p>
+      <div className="mt-20 flex flex-col gap-20 ">
+        <div>
+          <h2 className="text-xl my-3 font-medium ">
+            What is destination of choice?
+          </h2>
+          <GooglePlacesAutocomplete
+            apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}
+            selectProps={{
+              place,
+              onChange: (v) => {
+                setPlace(v);
+              },
+            }}
+          />
+        </div>
+        <div>
+          <h2 className="text-xl my-3 font-medium">
+            How many days are you planning your trip?
+          </h2>
+          <Input placeholder={"Ex.3"} type="number" />
+        </div>
+        <div>
+          <h2 className="text-xl my-3 font-medium">What is Your Budget?</h2>
+          <div className="grid grid-cols-3 gap-5 mt-5">
+            {SelectBudgetOptions.map((item, index) => (
+              <div
+                key={index}
+                className="p-4 border cursor-pointer rounded-lg hover:shadow"
+              >
+                <h2 className="text-4xl">{item.icon}</h2>
+                <h2 className="font-bold text-lg">{item.title}</h2>
+                <h2 className="text-sm text-gray-500">{item.desc}</h2>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div>
+          <h2 className="text-xl my-3 font-medium">Who do you plan on traveling with on your next adventure?</h2>
+          <div className="grid grid-cols-3 gap-5 mt-5">
+            {SelectTravelesList.map((item, index) => (
+              <div
+                key={index}
+                className="p-4 border cursor-pointer rounded-lg hover:shadow"
+              >
+                <h2 className="text-4xl">{item.icon}</h2>
+                <h2 className="font-bold text-lg">{item.title}</h2>
+                <h2 className="text-sm text-gray-500">{item.desc}</h2>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="my-10 flex justify-end">
+        <Button >Generate Trip</Button>
+      </div>
+    </div>
+  );
 }
 
-export default CreateTrip
+export default CreateTrip;
---
+++ src/constants/options.jsx
@@ -0,0 +1,71 @@
+export const SelectTravelesList = [
+  {
+    id: 1,
+    title: "Just Me",
+    desc: "A solo traveler exploring the world",
+    icon: "✈️",
+    people: "1 to 2 people",
+  },
+  {
+    id: 2,
+    title: "Couple",
+    desc: "Perfect for couples enjoying a romantic trip",
+    icon: "❤️",
+    people: "2 people",
+  },
+  {
+    id: 3,
+    title: "Family",
+    desc: "Traveling with family members and kids",
+    icon: "👨‍👩‍👧‍👦",
+    people: "3 to 5 people",
+  },
+  {
+    id: 4,
+    title: "Friends",
+    desc: "A fun trip with your friends group",
+    icon: "🎉",
+    people: "3 to 6 people",
+  },
+  {
+    id: 5,
+    title: "Business Trip",
+    desc: "Traveling for meetings or work purposes",
+    icon: "💼",
+    people: "1 to 3 people",
+  },
+];
+export const SelectBudgetOptions = [
+  {
+    id: 1,
+    title: "Cheap",
+    desc: "Stay conscious of cost",
+    icon: "💵",
+  },
+  {
+    id: 2,
+    title: "Budget",
+    desc: "Affordable travel with basic comfort",
+    icon: "💰",
+  },
+  {
+    id: 3,
+    title: "Mid-Range",
+    desc: "Balanced experience with comfort and value",
+    icon: "🏨",
+  },
+  {
+    id: 4,
+    title: "Luxury",
+    desc: "Premium travel with top-class services",
+    icon: "💎",
+  },
+  {
+    id: 5,
+    title: "Ultra Luxury",
+    desc: "Exclusive and high-end travel experience",
+    icon: "👑",
+  },
+];
+
+export const AI_PROMPT='Generate Travel Plan for Location : {location}'
