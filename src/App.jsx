a290319733829a32b567dfcf645a0965f11ce7f5
binyul <jiibinyul@gmail.com>
2026-03-26 10:43:32 +0545

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+    <h2>
+      <Button>Click</Button>
+    </h2>
+    </>
+  )
+}
+
+export default App
